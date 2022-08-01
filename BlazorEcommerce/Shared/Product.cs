3abfcaea0853f7a54ac60b5a7cd227a679934323
SE8148 <lms.seeney@gmail.com>
2022-08-01 12:52:24 +0100

    Select box for Product Variant

--- BlazorEcommerce/Shared/Product.cs
+++ BlazorEcommerce/Shared/Product.cs
@@ -15,6 +15,6 @@ namespace BlazorEcommerce.Shared
         public string ImageUrl { get; set; } = string.Empty;
         public Category? Category { get; set; }
         public int CategoryId { get; set; }
-        public List<ProductVariant> Variant { get; set; } = new List<ProductVariant>();
+        public List<ProductVariant> Variants { get; set; } = new List<ProductVariant>();
     }
 }
