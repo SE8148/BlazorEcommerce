9c7fab5dcf0afe3f50f2cf0715942bc9eba0e026
SE8148 <lms.seeney@gmail.com>
2022-08-09 16:43:05 +0100

    Add Search

--- BlazorEcommerce/Server/Services/ProductService/IProductService.cs
+++ BlazorEcommerce/Server/Services/ProductService/IProductService.cs
@@ -5,5 +5,7 @@
         Task<ServiceResponse<List<Product>>> GetProductsAsync();
         Task<ServiceResponse<Product>> GetProductAsync(int productId);
         Task<ServiceResponse<List<Product>>> GetProductsByCategory(string categoryId);
+        Task<ServiceResponse<List<Product>>> SearchProducts(string searchText);
+        Task<ServiceResponse<List<string>>> GetProductSearchSuggestions(string searchText);
     }
 }
